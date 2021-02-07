b70d55b1a32282a42e7a07aac2502f7d6dd36850
Lailz <lailaabdulraheem@gmail.com>
2021-02-07 13:14:09 +0300

    added delete route

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const tasks = require("./data");
+let tasks = require("./data");
 
 const app = express();
 
@@ -7,24 +7,32 @@ const app = express();
 //     // CODE
 // })
 
+// HOME: IM USELESS
 app.get("/home", (request, response) => {
   console.log("HOOOMMMMEEEEEEE");
   response.json({ message: "HELLO" });
 });
 
+// PRODUCTS LIST ROUTE
 app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
+// PRODUCT DETAIL ROUTE
 app.get("/tasks/:taskId", (req, res) => {
   console.log("🚀 ~ file: app.js ~ line 47 ~ app.get ~ req", req.params);
   const foundTask = tasks.find((task) => task.id === +req.params.taskId);
   if (foundTask) {
     res.json(foundTask);
   } else {
-    res.status(404);
-    res.json({ message: "Task Not Found" });
+    res.status(404).json({ message: "Task Not Found" });
   }
+});
+
+// PRODUCT DELETE ROUTE
+app.delete("/tasks/:taskId", (req, res) => {
+  tasks = tasks.filter((task) => task.id !== +req.params.taskId);
+  res.status(204).end();
 });
 
 app.listen(8001, () =>
